8a627ad00ff7191ef379f20eb833c282d8928fe5
adovg <kroatoan@i.ua>
2019-06-19 19:21:29 +0300

    !

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-function test() {
-	alert('hello js');
-	let sum = (firstNum + secondNum);
-	console.console.log(sum);
-};
-
-
-
-let firstNum = 5,
-	secondNum = 10;
-
-test();
+// function test() {
+// 	alert('hello js');
+// 	let sum = (firstNum + secondNum);
+// 	console.console.log(sum);
+// };
+//
+//
+//
+// let firstNum = 5,
+// 	secondNum = 10;
+//
+// test();
